7551caacf2a698f58c6020e90afa31564e4aabb6
MinecraftDawn <eric4902077@gmail.com>
2023-10-15 21:58:52 +0800

    加入SERVICE注入

--- tic-tac-toe/Hubs/TestHub.cs
+++ tic-tac-toe/Hubs/TestHub.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Numerics;
+using tic_tac_toe.Services;
 
 namespace tic_tac_toe.Hubs {
     public class TestHub : Hub {
+        private readonly TicTacToeService _service;
+
+        public TestHub(TicTacToeService service) {
+            _service = service;
+        }
 
         public async Task SendMessage(string user, string message) {
+            var state = _service.getGameState(user);
             Console.WriteLine(user + " : "+ message);
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", state);
         }
 
 
